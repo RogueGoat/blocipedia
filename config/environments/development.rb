43493564fc28811ea827304a94bdcbe25deb5a53
Samantha <roguegoatstudios@outlook.com>
2017-04-20 16:38:43 +0000

    checkpoint 2 devise user signup completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # DOUBLE CHECK IN CASE NEED TO CHANCE HOST
-  config.action_mailer.default_url_options = { host: 'https://bloc-foundation-roguegoat.c9users.io/', port: 8080 }
+  # config.action_mailer.default_url_options = { host: 'https://bloc-foundation-roguegoat.c9users.io/', port: 8080 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
