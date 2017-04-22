9570f5d3cedffb165878c8854b1164e96fafc503
Samantha <roguegoatstudios@outlook.com>
2017-04-22 05:30:19 +0000

    added html.erb files and working on wiki controller

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -4,5 +4,25 @@ class WikiController < ApplicationController
         @wiki = Wiki.new
     end
     
+    def index
+        @wiki = Wiki.all
+    end
+    
+    def create
+    end
+    
+    def show
+        Wiki.find(params[:id])
+    end
+    
+    def edit
+    end
+    
+    def update
+    end
+    
+    def destroy
+        @wiki = []
+    end
     
 end
