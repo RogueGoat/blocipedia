b351f8e3d6b4839c2c56faf6b87fb31772a7104d
Samantha <roguegoatstudios@outlook.com>
2017-04-23 09:16:55 +0000

    need to fix new wiki view

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -9,16 +9,40 @@ class WikiController < ApplicationController
     end
     
     def create
+
+     @wiki = Wiki.new
+     @wiki.title = params[:post][:title]
+     @wiki.body = params[:post][:body]
+
+     if @wiki.save
+       flash[:notice] = "Your new Wiki was saved"
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "Error! Wiki was not saved, please try again"
+       render :new
+     end
     end
     
     def show
-        Wiki.find(params[:id])
+        @wiki = Wiki.find(params[:id])
     end
     
     def edit
+        @wiki = Wiki.find(params[:id])
     end
     
     def update
+     @wiki = Wiki.find(params[:id])
+     @wiki.title = params[:post][:title]
+     @wiki.body = params[:post][:body]
+
+     if @wiki.save
+       flash[:notice] = "Your updates have been noted!"
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "Error! Wiki was not updated, please try again"
+       render :new
+     end
     end
     
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :models
+   devise_for :models
    devise_for :users
+   resources :wiki
    
-
+   get 'wiki' => 'wiki#index'
+   get 'about' => 'welcome#about'
    root 'welcome#index'
 end
