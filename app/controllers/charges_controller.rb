b30d2e6cd3913d490745dc4e3042ac3a8082fd07
Samantha <roguegoatstudios@outlook.com>
2017-05-05 07:15:57 +0000

    can upgrade

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,35 +1,31 @@
 class ChargesController < ApplicationController
 
   def create
-   # Creates a Stripe Customer object, for associating
-   # with the charge
-   customer = Stripe::Customer.create(
-     email: current_user.email,
-     card: params[:stripeToken]
-   )
- 
-   # Where the real magic happens
-   charge = Stripe::Charge.create(
-     customer: customer.id, # Note -- this is NOT the user_id in your app
-     amount: 1500,
-     description: "Premium Membership - #{current_user.email}",
-     currency: 'usd'
-   )
-   
    customer = Stripe::Customer.create(
       email: current_user.email,
       source: params[:stripeToken]
     )
     
-    current_user.update_attributes(stripe_id: customer.id)
+   charge = Stripe::Charge.create(
+     customer: customer.id,
+     amount: 1500,
+     description: "Premium Membership - #{current_user.email}",
+     currency: 'usd'
+    )
+    # subscription = Stripe::Subscription.create(
+    #   customer: customer.id,
+    #   plan: "Monthly Subscription for #{current_user.email}'s Premium Memberhip."
+    # )
+    
+    # current_user.update_attributes(stripe_id: customer.id)
+    # current_user.update_attributes(role: 'premium')
+    # current_user.update_attributes(stripe_subscription: subscription.id)
+    
     current_user.update_attributes(role: 'premium')
  
    flash[:notice] = "You've been upgraded to premium, #{current_user.email}!"
-   redirect_to root_path # or wherever
+   redirect_to root_path 
  
-   # Stripe will send back CardErrors, with friendly messages
-   # when something goes wrong.
-   # This `rescue block` catches and displays those errors.
    rescue Stripe::CardError => e
      flash[:alert] = e.message
      redirect_to new_charge_path
@@ -44,6 +40,13 @@ class ChargesController < ApplicationController
   end
   
   def destroy
-   current_user.role("standard")
+    customer = Stripe::Customer.find(
+      email: current_user.email,
+      source: params[:stripeToken]
+    )
+    customer.update_attributes(role: 'standard')
+    
+    flash[:notice] = "You've cancelled your subscription, #{current_user.email}! (peasant!)"
+    redirect_to root_path 
   end
 end
